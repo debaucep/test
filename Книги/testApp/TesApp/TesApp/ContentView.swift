f0edf4404a4f9c29983326df9b57bc93fd13fd03
DmitryShcherba <debaucep@yandex.ru>
2022-05-28 23:28:34 +0300

    testForGit

---
+++ Книги/testApp/TesApp/TesApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TesApp
+//
+//  Created by dmitry shcherba on 28.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+var a: String = "This is a test app for git"
---
+++ Книги/testApp/TesApp/TesApp/TesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesAppApp.swift
+//  TesApp
+//
+//  Created by dmitry shcherba on 28.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
